7ebd63df4fead87041a91572f1a6d4938b07eb58
Carlos <camegauk@gmail.com>
2026-09-03 00:09:26 +0200

    Actualiza la web

--- sw.js
+++ sw.js
@@ -24,7 +24,7 @@
    No se precarga kanji-datos.js: son 1,4 MB, y quien solo abre el silabario no
    tiene por qué gastarlos. Se guarda la primera vez que se usa. */
 
-const VERSION = '75675dac6e40';
+const VERSION = '3377134ccd00';
 const CACHE = 'aprende-japones-' + VERSION;
 
 /* El armazón: lo mínimo para que las tres páginas abran sin red. */
