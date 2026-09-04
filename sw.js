1082600f449d3904c0fa5f784db2e0f6859ebb44
Carlos <camegauk@gmail.com>
2026-09-05 00:49:08 +0200

    Actualiza la web

--- sw.js
+++ sw.js
@@ -35,7 +35,7 @@
 
    El manual sí va: es un HTML de un solo archivo, sin dependencias. */
 
-const VERSION = 'fa3e02e2a462';
+const VERSION = 'c049ddc05332';
 const CACHE = 'aprende-japones-' + VERSION;
 
 /* El armazón: lo mínimo para que las tres páginas abran sin red. */
